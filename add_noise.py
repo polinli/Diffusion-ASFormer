3e903de352875dec2beed5d6809fa2917c4b0e57
polinli <mendelmaker>
2023-06-06 02:14:13 +0800

    add generate_noise() for inference

--- add_noise.py
+++ add_noise.py
@@ -1,24 +1,5 @@
 import torch
 
-def add_noise(y0, s=1000, beta_start=0.0001, beta_end=0.02, beta_schedule='linear'):
-    L, C = y0.shape
-    epsilon = torch.randn(L, C)
-
-    # Define beta schedule
-    beta = torch.linspace(beta_start, beta_end, steps=s)
-
-    # Compute alpha bar
-    alpha_bar = torch.cumprod(1 - beta, dim=0)
-
-    # Compute corrupted sequence Y_s
-    y_s = torch.sqrt(alpha_bar[s-1]) * y0 + torch.sqrt(1 - alpha_bar[s-1]) * epsilon
-
-    # Normalize action sequences
-    y_s = 2 * (y_s - y_s.min()) / (y_s.max() - y_s.min()) - 1
-
-    return y_s
-
-
 def forwar_process(batch_target: Tensor, total_steps: Tensor, beta_start = 0.0001, beta_end = 0.04, beta_steps) -> Tensor:
     # simplified e forward process using the reparameterization trick
     # batch_target shape: (N, L), add noise on all the elements
@@ -36,3 +17,10 @@ def forwar_process(batch_target: Tensor, total_steps: Tensor, beta_start = 0.000
     eps = torch.randn_like(batch_target)
 
     return mean + deviation * eps
+
+def generate_noise(batch_size, length):
+    # generate pure gaussion noise with fixed random seed
+    torch.manual_seed(19990526)
+    noise = torch.randn(batch_size, length)
+    
+    return noise
